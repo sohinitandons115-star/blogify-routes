5e612f4b23411852f1706d65a5bdfbe1a73b80b0
sohinitandons115-star <sohini.tandon.s.115@kalvium.com>
2026-01-21 15:54:38 +0530

    feat: modularize post routes using express.Router

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const postRouter = require('./routes/posts.routes');
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blogify API!');
+});
+
+// Mount router
+app.use('/api/v1/posts', postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
